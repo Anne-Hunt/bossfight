bcfcb51a9d2910c0ebc7d4552e98241bee09b819
Anne Hunt <acathunt@gmail.com>
2024-03-21 14:14:18 -0600

    basic HTML and CSS

---
+++ app.js
@@ -0,0 +1,28 @@
+const heroes = [
+    {
+        name: 'Slate Slabrock',
+        type: 'dwarf',
+        damage: 5,
+        health: 100,
+        gold: 0,
+        level: 1
+    },
+    {
+        name: 'Flint Ironstag',
+        type: 'elf',
+        damage: 10,
+        health: 50,
+        gold: 0,
+        level: 1
+    }
+]
+
+const boss = {
+    health: 100,
+    maxHealth: 100,
+    damage: 5,
+    level: 1,
+    money: 50
+}
+
+
