505d02ca566beee5c6b395e23f7b4262831f82b6
Anne Hunt <161772537+Anne-Hunt@users.noreply.github.com>
2024-03-21 21:32:23 -0600

    Added function elements

--- app.js
+++ app.js
@@ -4,14 +4,32 @@ const heroes = [
         type: 'dwarf',
         damage: 5,
         health: 100,
-        level: 1
+        level: 1,
+        active: 'true'
     },
     {
         name: 'Flint Ironstag',
         type: 'elf',
         damage: 10,
         health: 50,
-        level: 1
+        level: 1,
+        active: 'true'
+    },
+    {
+        name: 'Brock Rockblang',
+        type: 'troll',
+        damage: 10,
+        health: 75,
+        level: 2,
+        active: 'false'
+    },
+    {
+        name: 'Ronvald Stormcracker',
+        type: 'wizard',
+        damage: 10,
+        health: 150,
+        level: 4,
+        active: 'false'
     }
 ]
 
@@ -36,6 +54,13 @@ function drawGame() {
     let sidekickLevelElem = document.getElementById('sidekick-level')
     let bossCountElem = document.getElementById('boss-count')
 
+    goldElem.innerHTML = `<span>Gold: ${gold}</span>`
+    heroHealthElem.innerHTML = `<span>Health: ${heroes[0].health}</span>`
+    heroLevelElem.innerHTML = `<span>Level: ${heroes[0].level}</span>`
+    sidekickHealthElem.innerHTML = `<span>Health: ${heroes[1].health}</span>`
+    sidekickLevelElem.innerHTML = `<span>Level: ${heroes[1].level}</span>`
+    bossCountElem.innerHTML = `<span>Bosses Slain: ${killedBosses}</span>`
+
     bossDead()
     checkHero()
 }
@@ -43,7 +68,9 @@ function drawGame() {
 function attackBoss() {
     let stab = 0
     for (let i = 0; i < heroes.length; i++) {
-        stab += heroes[i].damage
+        if (heroes[i].active == 'true') {
+            stab += heroes[i].damage
+        }
     }
     console.log('stab', stab)
     drawGame()
@@ -55,36 +82,39 @@ function bossAttacks() {
         heroes[i].health -= boss.damage
         console.log('attacked!', heroes[i].health)
     }
-    drawGame()
 }
 
 setInterval(bossAttacks, 5000)
 
 function bossDead() {
-    if (boss.health <= 0) {
+    if (boss.health < 0) {
         window.alert('You slayed the monster!')
         boss.maxHealth += 50;
         gold += boss.money;
         boss.level++;
         boss.health = boss.maxHealth;
         killedBosses++;
+        drawGame()
     }
-    drawGame()
 }
 
 function healHero(heroName) {
     if (gold >= 10) {
         heroes.find(hero => hero.name == heroName)
-        hero.health += 10;
+        heroName.health += 10;
         gold -= 10;
+        drawGame()
+    } else {
+        window.alert('You are too poor!')
     }
-    drawGame()
 }
 
 function checkHero() {
     for (let i = 0; i < heroes.length; i++) {
         if (heroes[i].health <= 0) {
-            window.alert
+            window.alert('You lost a member of your party!');
+            heroes[i].damage = 0;
+            heroes[i].active = 'false';
         }
     }
 }
